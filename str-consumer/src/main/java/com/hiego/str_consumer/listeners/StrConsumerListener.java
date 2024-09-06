4bfebbb1d5a41354492a7324c7d7f9295811b461
Hiego <hiegobarreto@gmail.com>
2024-09-06 10:52:05 -0300

    custom listener

--- str-consumer/src/main/java/com/hiego/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/hiego/str_consumer/listeners/StrConsumerListener.java
@@ -1,20 +1,27 @@
 package com.hiego.str_consumer.listeners;
 
+import com.hiego.str_consumer.custom.StrConsumerCustomListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Message received: {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
         log.info("LOG ::: Message received: {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Message received: {}", message);
     }
 }
